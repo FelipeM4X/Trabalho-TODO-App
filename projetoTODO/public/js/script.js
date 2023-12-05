2b32de8cf760fcac4d53a3d4bbb0432a40357911
Luís Felipe <99267632+FelipeM4X@users.noreply.github.com>
2023-12-05 17:38:03 -0300

    Add files via upload

--- projetoTODO/public/js/script.js
+++ projetoTODO/public/js/script.js
@@ -16,10 +16,23 @@ function descompletarTarefa(id) {
         headers:{
             'Content-Type': 'application/json'
         },
+    
         body: JSON.stringify({ id })
     })
 
     window.location.reload()
+}
+
+function excluirTarefa(id) {
+    fetch("http://localhost:3000/excluir", {
+        method:"POST",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        
+        body: JSON.stringify({ id })
+    })
+
 }
 
 
