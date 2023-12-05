e2e6a841c230bd415dfc54443cbaf9b192a653f2
Luís Felipe <99267632+FelipeM4X@users.noreply.github.com>
2023-12-05 17:13:39 -0300

    Add files via upload

--- projetoTODO/index.js
+++ projetoTODO/index.js
@@ -1,3 +1,12 @@
 const express = requeire("express")
 
 const app = express()
+const aplicativo = express()
+
+app.get ( '/' ,  ( requisição ,  resposta )  =>  {
+    resposta . enviar ( "Olá, Mundo!" )
+} )
+
+app.listen( 3000 ,  ( )  =>  {
+    console . log ( "Servidor rodando na porta 3000!" )
+} )
