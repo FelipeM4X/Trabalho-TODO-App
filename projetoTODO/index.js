82e8682f4a0b1db45234c4b4660a8fe83f0fcb36
Luís Felipe <99267632+FelipeM4X@users.noreply.github.com>
2023-12-05 17:17:03 -0300

    Add files via upload

--- projetoTODO/index.js
+++ projetoTODO/index.js
@@ -1,27 +1,17 @@
-const express = requeire("express")
+const express = require("express")
 const  exphbs  =  require ("express-handlebars" )
 
-const aplicativo = express()
+const app = express()
 
-app.get ( '/' ,  ( requisição ,  resposta )  =>  {
-    resposta . enviar ( "Olá, Mundo!" )
 
-app.engine ('handlebars',exphbs.engine)
+app.engine ('handlebars',exphbs.engine())
 app.set ('view engine','handlebars')
-})
+
+
 app.get( '/',(requisicao, resposta )  =>  {
-    resposta.send ( "Olá, Mundo!" )
     resposta.render('home')
 } )
 
-
-app.get( '/',(requisicao, resposta )  =>  {
-    resposta.send ( "Olá, Mundo!" )
-} )
-
-app.listen( 3000 ,  ( )  =>  {
-    console . log ( "Servidor rodando na porta 3000!" )
-})
-app.listen( 3000, ()  =>  {
+app.listen(3000,() => {
     console.log ("Servidor rodando na porta 3000!")
-})
+} )
