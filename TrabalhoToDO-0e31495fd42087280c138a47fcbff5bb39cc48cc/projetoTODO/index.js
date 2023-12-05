6e3e2e25e1db14f2a0dcf8f6260153f5045f937a
Luís Felipe <99267632+FelipeM4X@users.noreply.github.com>
2023-12-05 17:35:38 -0300

    Add files via upload

---
+++ TrabalhoToDO-0e31495fd42087280c138a47fcbff5bb39cc48cc/projetoTODO/index.js
@@ -0,0 +1,152 @@
+const express = require("express")
+const  exphbs  =  require ("express-handlebars" )
+const mysql = require("mysql2")
+
+const app = express()
+
+
+app.engine ('handlebars',exphbs.engine())
+app.set ('view engine','handlebars')
+
+app.use(express.static('public'))
+
+// converter dados do formulário em objeto javascript
+app.use(express.urlencoded ({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+
+app.post('/completar',(requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+    UPDATE tarefas
+    SET completa = '1'
+    WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if(erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+
+})
+
+app.post('/descompletar',(requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+    UPDATE tarefas
+    SET completa = '0'
+    WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if(erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+
+})
+
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+       INSERT INTO tarefas (descricao, completa)
+       VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if(erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+
+    app.get( '/ativas',(requisicao, resposta )  =>  {
+        const sql = ` 
+        SELECT * FROM tarefas
+        WHERE completa = 0
+        `
+   
+        conexao.query(sql, (erro, dados) => {
+            if (erro){
+                return console.log(erro)
+            }
+    
+            const tarefas = dados.map((dado) => {
+                return{
+                    id: dado.id,
+                    descricao: dado.descricao,
+                    completa: false
+                }
+            })
+    
+            const  quantidadeTarefasAtivas = tarefasAtivas.length
+            
+            resposta.render('home', {tarefas, tarefasAtivas })
+        })
+    } )
+
+
+    
+})
+
+app.get( '/',(requisicao, resposta )  =>  {
+    const sql = ' SELECT * FROM tarefas'
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro){
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) => {
+            return{
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0 ? false : true,
+            }
+        })
+
+        const tarefasAtivas = tarefas.filter((tarefa) => {
+            return tarefa.completa === false && tarefa
+        })
+
+        const  quantidadeTarefasAtivas = tarefasAtivas.length
+        
+        resposta.render('home', {tarefas, quantidadeTarefasAtivas })
+    })
+} )
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "todo-app",
+    port: 3306
+})
+
+
+conexao.connect((erro) => {
+    if(erro) {
+        return console.log(erro)
+    }
+
+    console.log("Estou conectado ao MySql.")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000")
+    })
+
+})
